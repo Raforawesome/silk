11fd498b70b96c4495a2fba3d25fe8e54015c7ef
Raforawesome <ubuntuhasnoroblox@gmail.com>
2026-02-02 14:45:41 -0500

    ToBytes trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
+use std::io::Write;
+
 pub mod http;
+
+pub trait ToBytes {
+    fn write_to(&self, buffer: &mut impl Write) -> std::io::Result<()>;
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut buffer = Vec::new();
+        self.write_to(&mut buffer).unwrap();
+        buffer
+    }
+}
