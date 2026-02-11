f05b7a2e06d53d465e9c7fe2375ce0ca43de0bda
Raforawesome <ubuntuhasnoroblox@gmail.com>
2026-02-11 13:20:16 -0500

    impl TryFrom byte slice methods for enums

--- src/http.rs
+++ src/http.rs
@@ -15,6 +15,31 @@ pub enum Version {
 pub enum Method {
     Post,
     Get,
+    Put,
+}
+
+impl TryFrom<&[u8]> for Version {
+    type Error = Code;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        match value {
+            b"HTTP/1.1" => Ok(Version::Http1_1),
+            _ => Err(Code::HttpVersionNotSupported),
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for Method {
+    type Error = Code;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        match value {
+            b"POST" => Ok(Method::Post),
+            b"GET" => Ok(Method::Get),
+            b"PUT" => Ok(Method::Put),
+            _ => Err(Code::BadRequest),
+        }
+    }
 }
 
 impl ToBytes for Version {
